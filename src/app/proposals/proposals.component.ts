deb0c332929d9887073bff2bc4136be7f37a9651
Omer64kingwork <OemerFaruk.Yesil@draexlmaier.com>
2023-11-29 14:20:40 +0100

    commit

--- src/app/proposals/proposals.component.ts
+++ src/app/proposals/proposals.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ProposalsComponent implements OnInit {
   names = ["Jeff", "Kevin", "John", "joe"];
   texts = ["hamster king", "realest hamster", "got a hamsterghini", "i woke up in a new bughatti"];
-  images = ["./assets/img/hamster/5.jpg", "./assets/img/hamster/4.jpg", "./assets/img/hamster/7.jpg", "./assets/img/hamster/8.jpg"];
+  images = ["docs/assets/img/hamster/5.jpg", "docs/assets/img/hamster/4.jpg", "docs/assets/img/hamster/7.jpg", "docs/assets/img/hamster/8.jpg"];
 
   constructor() {}
 
