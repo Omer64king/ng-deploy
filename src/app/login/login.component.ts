3ba3b1d42d39bae828d71f71c219d13489e93dd5
Omer64king <unity64king2@gmail.com>
2023-12-21 14:39:53 +0100

    added a login system

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,8 +3,10 @@ import {NgForm} from '@angular/forms';
 import { FriendService } from '../friend.service';
 import fetch from 'node-fetch';
 import { Client } from 'pg';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
+import {Request, Response} from 'express'; 
 import { get } from 'http';
+import { json } from 'stream/consumers';
 
 
 
@@ -23,10 +25,20 @@ import { get } from 'http';
 export class LoginComponent {      
  
 
+  i: any; 
+  passwordright: boolean; 
+  emailright: boolean;
+  passwordright2: boolean; 
+  emailright2: boolean;
+
   
+  jsonsting: any; 
+  checkarray: any; 
   email: any; 
   password: any; 
   getvalue: any; 
+  Jsondata: any; 
+  templateInJsonFormat: any;
  
 
   constructor(public fs: FriendService, private http: HttpClient) {} 
@@ -36,13 +48,75 @@ export class LoginComponent {
     
   }
   Authentication() {
-    
-  this.http.get('postgresql://postgres:2929@localhost:5432/test').subscribe((data) => {
-    console.log(data);
-  });
+    this.http.get('http://localhost:7300/credentials').subscribe((data) => {
+    this.passwordright = false; 
+    this.emailright = false; 
+      //console.log(Object.values(data));
+        this.jsonsting = JSON.stringify(data);
 
-    
+        this.templateInJsonFormat = JSON.parse(this.jsonsting);
+
+
+        this.checkarray = this.templateInJsonFormat.password1
+        console.log(this.templateInJsonFormat.countries[0].emaillogin);
+
+
+        for (this.i = 0; this.i < this.templateInJsonFormat.countries.length; this.i++) {
+          if (this.templateInJsonFormat.countries[this.i].emaillogin == this.email) {
+            console.log("ye"); 
+            return this.emailright = true;
+          }
+          if (this.templateInJsonFormat.countries[this.i].passwordlogin == this.password) {
+            return this.passwordright = true;
+          }
+            
+        }
+        return false;
+    });
   }
- 
+
+  Registration() {
+    this.http.get('http://localhost:7300/credentials').subscribe((data) => {
+      this.passwordright2 = false; 
+      this.emailright2 = false; 
+        //console.log(Object.values(data));
+          this.jsonsting = JSON.stringify(data);
+  
+          this.templateInJsonFormat = JSON.parse(this.jsonsting);
+  
+  
+          this.checkarray = this.templateInJsonFormat.password1
+          console.log(this.templateInJsonFormat.countries[0].emaillogin);
+  
+  
+          for (this.i = 0; this.i < this.templateInJsonFormat.countries.length; this.i++) {
+            if (this.templateInJsonFormat.countries[this.i].emaillogin == this.email && this.templateInJsonFormat.countries[this.i].passwordlogin == this.password) {
+              console.log("ye"); 
+              this.emailright2 = true;
+              this.passwordright2 = true;
+              return; 
+            }
+            if (this.passwordright2 == false && this.emailright2 == false)
+            {
+              this.passwordright2 = false;
+              this.emailright2 = false;
+            }
+          }
+          const body = { emaillogin: this.email, passwordlogin: this.password}; 
+
+          console.log(this.passwordright2); 
+          if (this.passwordright2 == false && this.emailright2 == false)
+          {
+            this.http.post('http://localhost:7300/credentials', body, ).subscribe((data) => {
+              console.log(body);
+            });
+          }
+      });
+     
+  }
+
+  
+
+  
 }
 
